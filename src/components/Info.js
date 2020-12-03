c77963b5cae45b7d9bc6c386d91465894fbb33c0
MRFolio <roose.markus@gmail.com>
2020-12-03 23:33:40 +0200

    inital commit

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { GithubContext } from '../context/context';
-import styled from 'styled-components';
-import { GoRepo, GoGist } from 'react-icons/go';
-import { FiUsers, FiUserPlus } from 'react-icons/fi';
+import React from "react";
+import { GithubContext } from "../context/context";
+import styled from "styled-components";
+import { GoRepo, GoGist } from "react-icons/go";
+import { FiUsers, FiUserPlus } from "react-icons/fi";
 
 const UserInfo = () => {
-  return <h2>user info component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const { public_repos, followers, following, public_gists } = githubUser;
+  return <h2>user info component: </h2>;
 };
 
 const Wrapper = styled.section`
