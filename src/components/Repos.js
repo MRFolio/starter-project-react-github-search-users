c472169027ec9ef63e1b23c343aff466d720d39c
MRFolio <roose.markus@gmail.com>
2020-12-05 18:23:27 +0200

    feat(github-users): halfway done add reduce

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,59 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = React.useContext(GithubContext);
+  const languages = repos.reduce((acc, item) => {
+    const { language, stargazers_count: stars } = item;
+    if (!language) return acc;
+    if (!acc[language]) {
+      acc[language] = { label: language, value: 1, stars };
+    } else {
+      acc[language] = {
+        ...acc[language],
+        value: acc[language].value + 1,
+        stars: acc[language].stars + stars,
+      };
+    }
+
+    return acc;
+  }, {});
+
+  const mostUsed = Object.values(languages)
+    .sort((a, b) => b.value - a.value)
+    .slice(0, 5);
+
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => b.stars - a.stars)
+    .slice(0, 5);
+
+  const chartData = [
+    {
+      label: "HTML",
+      value: "13",
+    },
+    {
+      label: "CSS",
+      value: "160",
+    },
+    {
+      label: "Javascript",
+      value: "80",
+    },
+  ];
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {/* <ExampleChart data={chartData} />; */}
+        <Pie3D data={mostUsed} />
+        <div></div>
+        <Doughnut2D data={chartData} />
+        <div></div>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
