ac4291d35b028b02116ed1d998c6e4270ae3735c
MRFolio <roose.markus@gmail.com>
2020-12-07 23:23:31 +0200

    feat(github-user): completed project

--- src/context/context.js
+++ src/context/context.js
@@ -3,6 +3,7 @@ import mockUser from "./mockData.js/mockUser";
 import mockRepos from "./mockData.js/mockRepos";
 import mockFollowers from "./mockData.js/mockFollowers";
 import axios from "axios";
+import { MdRateReview } from "react-icons/md";
 
 const rootUrl = "https://api.github.com";
 
@@ -12,9 +13,81 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+  const [requests, setRequests] = useState(0);
+  const [loading, setLoading] = useState(false);
+  //error
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+
+  const searchGithubUser = async (user) => {
+    toggleError();
+    setLoading(true);
+    try {
+      const response = await fetch(`${rootUrl}/users/${user}`);
+      const data = await response.json();
+
+      if (data) {
+        setGithubUser(data);
+        const { login, followers_url } = data;
+        const fetcMore = async () => {
+          const response2 = await fetch(
+            `${rootUrl}/users/${login}/repos?per_page=100`
+          );
+          const data2 = await response2.json();
+          setRepos(data2);
+        };
+        fetcMore();
+
+        const fetchMore2 = async () => {
+          const response3 = await fetch(`${followers_url}?per_page=100`);
+          const data3 = await response3.json();
+          setFollowers(data3);
+        };
+        fetchMore2();
+      } else {
+        toggleError(true, "there is no user with that username");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      checkRequests();
+      setLoading(false);
+    }
+  };
+
+  const checkRequests = async () => {
+    try {
+      const response = await fetch(`${rootUrl}/rate_limit`);
+      const {
+        rate: { remaining },
+      } = await response.json();
+
+      if (remaining === 0) {
+        toggleError(true, "sorry");
+        // error
+      }
+      setRequests(remaining);
+    } catch (error) {}
+  };
+  useEffect(() => {
+    checkRequests();
+  }, []);
 
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+        loading,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,20 @@ import React from "react";
 import { Info, Repos, User, Search, Navbar } from "../components";
 import loadingImage from "../images/preloader.gif";
 import { GithubContext } from "../context/context";
+import { useContext } from "react";
 const Dashboard = () => {
+  const { loading } = useContext(GithubContext);
+
+  if (loading) {
+    return (
+      <main>
+        <Navbar />
+        <Search />
+        <img className="loading-img" src={loadingImage} alt="loading" />
+      </main>
+    );
+  }
+
   return (
     <main>
       <Navbar></Navbar>
